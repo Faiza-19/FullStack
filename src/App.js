50e0126d3bdc46a94d7b34c18ee798fc38926b0b
Faiza-19 <faizahameed04@gmail.com>
2024-12-03 17:07:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React,{useState} from 'react';
+import solid from './solid.jpg';
+import pattern from './pattern.jpg';
+function App()
+{
+  const[c1,setc1]=useState(0);
+  const[c2,setc2]=useState(0);
+  function chgc1()
+  {
+    setc1(function(e)
+  {
+    return e+1;
+  });
+  }
+  function chgc2()
+  {
+    setc2(function(e)
+  {
+    return e+1;
+  });
+  }
+  return(
+    <div style={{ backgroundColor: "lavender", height: "100vh", padding: "20px" }}>
+      <center>
+        <h1>What is Trending?</h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Style</th>
+            <th>vote</th>
+            <th>count</th>
+          </tr>
+          <tr>
+            <td>Solid</td>
+            <td><img src={solid}/></td>
+            <td><button type="vote" onClick={chgc1}>vote</button></td>
+            <td>{c1}
+            </td>
+          </tr>
+          <tr>
+            <td>Pattern</td>
+            <td><img src={pattern}/></td>
+            <td><button type="vote" onClick={chgc2}>vote</button></td>
+            <td>{c2}
+            </td>
+          </tr>
+        </table>
+      </center>
     </div>
-  );
+  )
 }
-
 export default App;
+
